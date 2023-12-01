d44e1300420b406865f250d0ce6bb0d1dfad09c4
StygianTendency <king11213rb@gmail.com>
2023-11-30 20:36:10 -0600

    UAClient Added

--- DSFinalProject/Client/UAClient.java
+++ DSFinalProject/Client/UAClient.java
@@ -1,3 +1,7 @@
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.net.Socket;
+
 /********************************
  Name: Joshua C, David T, Christopher M
  Team: 3
@@ -6,7 +10,19 @@
  ********************************/
 public class UAClient {
 
-    public static void main(String[] args) {
+    
+    private static final String CENTRAL_SERVER_IP = "localhost";
+    private static final int CENTRAL_SERVER_PORT = 479;
 
+    public static void main(String[] args) {
+        try (Socket socket = new Socket(CENTRAL_SERVER_IP, CENTRAL_SERVER_PORT);
+             ObjectInputStream in = new ObjectInputStream(socket.getInputStream())) {
+
+            // Implement logic for printing out the messages it receives from UACentralServer and FittingRoomServer
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
+
 }
