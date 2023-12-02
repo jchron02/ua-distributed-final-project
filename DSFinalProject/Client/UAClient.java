9e4f8d67ccce40ef4187921eb25e0fed596660ce
StygianTendency <king11213rb@gmail.com>
2023-12-02 01:14:51 -0600

    Hopefully allowed client to recieve messages from central server & FittingRoom.

--- DSFinalProject/Client/UAClient.java
+++ DSFinalProject/Client/UAClient.java
@@ -2,15 +2,8 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.net.Socket;
 
-/********************************
- Name: Joshua C, David T, Christopher M
- Team: 3
- Final Project
- Due Date: 
- ********************************/
 public class UAClient {
 
-    
     private static final String CENTRAL_SERVER_IP = "localhost";
     private static final int CENTRAL_SERVER_PORT = 479;
 
@@ -18,11 +11,20 @@ public class UAClient {
         try (Socket socket = new Socket(CENTRAL_SERVER_IP, CENTRAL_SERVER_PORT);
              ObjectInputStream in = new ObjectInputStream(socket.getInputStream())) {
 
-            // Implement logic for printing out the messages it receives from UACentralServer and FittingRoomServer
+            // Display the confirmation message from the server
+            String confirmationMessage = (String) in.readObject();
+            System.out.println("Server says: " + confirmationMessage);
 
-        } catch (IOException e) {
+            // Keep the client connected and waiting for server messages
+            while (true) {
+                String serverMessage = (String) in.readObject();
+                if (serverMessage != null) {
+                    System.out.println("Server says: " + serverMessage);
+                }
+            }
+
+        } catch (IOException | ClassNotFoundException e) {
             e.printStackTrace();
         }
     }
-
 }
--- DSFinalProject/Server/UACentralServer.java
+++ DSFinalProject/Server/UACentralServer.java
@@ -2,83 +2,99 @@ import java.io.*;
 import java.net.*;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.logging.*;
 
 public class UACentralServer {
+
+    private static final CopyOnWriteArrayList<ObjectOutputStream> clients = new CopyOnWriteArrayList<>();
 
     public static void main(String[] args) {
         int portNumber = 479;
         try {
             ServerSocket serverSocket = new ServerSocket(portNumber);
-            System.out.println("UAServer is running and listening on port " + portNumber +"...");
+            System.out.println("UAServer is running and listening on port " + portNumber + "...");
 
             while (true) {
                 Socket clientSocket = serverSocket.accept();
                 System.out.println("Client connected: " + clientSocket.getInetAddress().getHostAddress());
+
+                ObjectOutputStream out = new ObjectOutputStream(clientSocket.getOutputStream());
+
+                // Send confirmation message to the client
+                out.writeObject("You are now connected to the server!");
+
+                clients.add(out);
+
+                // Handle client input in a separate thread
+                new Thread(new HandleClientInput(clientSocket, out)).start();
             }
         } catch (IOException e) {
             System.err.println("Error: " + e.getMessage());
         }
     }
-}
 
-class HandleClientInput implements Runnable {
-    private Socket clientSocket;
-    private Logger logger;
+    static class HandleClientInput implements Runnable {
+        private Socket clientSocket;
+        private ObjectOutputStream clientOut;
+        private Logger logger;
 
-    public HandleClientInput(Socket socket) {
-        this.clientSocket = socket;
-        configureLogger();
-    }
-
-    private void configureLogger() {
-        this.logger = Logger.getLogger(HandleClientInput.class.getName());
-
-        try {
-            FileHandler fileHandler = new FileHandler("UAServer.log", true);
-            fileHandler.setFormatter(new SimpleFormatter());
-            logger.addHandler(fileHandler);
-        } catch (IOException e) {
-            System.err.println("Error setting up logger: " + e.getMessage());
+        public HandleClientInput(Socket socket, ObjectOutputStream out) {
+            this.clientSocket = socket;
+            this.clientOut = out;
+            configureLogger();
         }
-    }
 
-    @Override
-    public void run() {
-        try (PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-             BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
+        private void configureLogger() {
+            this.logger = Logger.getLogger(HandleClientInput.class.getName());
 
-            logInfo("New client connection from IP address " + clientSocket.getInetAddress().getHostAddress());
-
-            String inputLine;
-            while ((inputLine = in.readLine()) != null) {
-                System.out.println("Received message from client: " + inputLine);
-                logInfo("Received message from client: " + inputLine);
-
-                // Start fittingroom task.
-            }
-
-            logInfo("Client " + clientSocket.getInetAddress().getHostAddress() + " disconnected");
-
-        } catch (IOException e) {
-            System.err.println("Error: " + e.getMessage());
-            logWarning("Error : " + e.getMessage() + " received from response from client " + clientSocket.getInetAddress().getHostAddress());
-        } finally {
             try {
-                clientSocket.close();
+                FileHandler fileHandler = new FileHandler("UAServer.log", true);
+                fileHandler.setFormatter(new SimpleFormatter());
+                logger.addHandler(fileHandler);
             } catch (IOException e) {
-                System.err.println("Error closing client socket: " + e.getMessage());
-                logWarning("Error closing client socket: " + e.getMessage());
+                System.err.println("Error setting up logger: " + e.getMessage());
             }
         }
-    }
 
-    private void logInfo(String message) {
+        @Override
+        public void run() {
+            try (ObjectInputStream in = new ObjectInputStream(clientSocket.getInputStream())) {
 
-        logger.log(Level.INFO, "[" + new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()) + "] INFO: " + message);
-    }
+                logInfo("New client connection from IP address " + clientSocket.getInetAddress().getHostAddress());
 
-    private void logWarning(String message) {
-        logger.log(Level.WARNING, "[" + new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()) + "] WARNING: " + message);
+                // Handle messages from the client
+                while (true) {
+                    String clientMessage = (String) in.readObject();
+                    System.out.println("Received message from client: " + clientMessage);
+                    logInfo("Received message from client: " + clientMessage);
+
+                    // Broadcast the message to all connected clients
+                    for (ObjectOutputStream client : clients) {
+                        client.writeObject("Client " + clientSocket.getInetAddress().getHostAddress() + ": " + clientMessage);
+                    }
+                }
+
+            } catch (IOException | ClassNotFoundException e) {
+                System.err.println("Error: " + e.getMessage());
+                logWarning("Error : " + e.getMessage() + " received from response from client " + clientSocket.getInetAddress().getHostAddress());
+            } finally {
+                try {
+                    clients.remove(clientOut);
+                    clientSocket.close();
+                } catch (IOException e) {
+                    System.err.println("Error closing client socket: " + e.getMessage());
+                    logWarning("Error closing client socket: " + e.getMessage());
+                }
+            }
+        }
+
+        private void logInfo(String message) {
+            logger.log(Level.INFO, "[" + new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()) + "] INFO: " + message);
+        }
+
+        private void logWarning(String message) {
+            logger.log(Level.WARNING, "[" + new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()) + "] WARNING: " + message);
+        }
     }
 }
